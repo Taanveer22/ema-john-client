65a29f9c9a6c0c4095e39eff68cf4ec531ef717a
Taanveer22 <taanveer469@gmail.com>
2026-06-11 03:39:39 +0600

    client side fix done

---
+++ src/main.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Checkout from './components/Checkout/Checkout';
+import Inventory from './components/Inventory/Inventory';
+import Root from './components/Layout/Root';
+import Login from './components/Login/Login';
+import Orders from './components/Orders/Orders';
+import Shop from './components/Shop/Shop';
+import cartProductsLoader from './loaders/cartProductsLoader';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root></Root>,
+    children: [
+      {
+        index: true,
+        element: <Shop></Shop>,
+      },
+      {
+        path: 'orders',
+        element: <Orders></Orders>,
+        loader: cartProductsLoader,
+      },
+      {
+        path: 'inventory',
+        element: <Inventory></Inventory>,
+      },
+      {
+        path: 'checkout',
+        element: <Checkout></Checkout>,
+      },
+      {
+        path: 'login',
+        element: <Login></Login>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
