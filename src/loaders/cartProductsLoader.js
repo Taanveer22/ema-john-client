7eff0c420a771619658723582bbad2c22639e7c0
Taanveer22 <taanveer469@gmail.com>
2026-06-13 03:52:43 +0600

    alhamdulliah create operation done for orders page

--- src/loaders/cartProductsLoader.js
+++ src/loaders/cartProductsLoader.js
@@ -1,29 +1,39 @@
-import { getShoppingCart } from "../utilities/fakedb";
+import { getShoppingCart } from '../utilities/fakedb';
 
 const cartProductsLoader = async () => {
-    const loadedProducts = await fetch('http://localhost:5000/products');
-    const products = await loadedProducts.json();
+  // if cart data is in database, you have to use async await
+  const storedCart = getShoppingCart();
+  console.log(storedCart);
+  const storedCartIdList = Object.keys(storedCart);
+  console.log(storedCartIdList);
 
-    // if cart data is in database, you have to use async await
-    const storedCart = getShoppingCart();
+  const loadedProducts = await fetch('http://localhost:5000/productsById', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(storedCartIdList),
+  });
+  const productsData = await loadedProducts.json();
+  console.log(productsData);
 
-    const savedCart = [];
+  const savedCart = [];
 
-    for (const id in storedCart) {
-        const addedProduct = products.find(pd => pd._id === id);
-        if (addedProduct) {
-            const quantity = storedCart[id];
-            addedProduct.quantity = quantity;
-            savedCart.push(addedProduct);
-        }
+  for (const id in storedCart) {
+    const addedProduct = productsData.find((product) => product._id === id);
+    if (addedProduct) {
+      const quantity = storedCart[id];
+      addedProduct.quantity = quantity;
+      savedCart.push(addedProduct);
     }
+  }
 
-    // if you need to send two things
-    // return [products, savedCart]
-    // another options
-    // return { products, cart: savedCart }
+  // if you need to send two things
+  // return [productsData, savedCart]
+  // another options
+  // return { productsData, cart: savedCart }
 
-    return savedCart;
-}
+  return savedCart;
+};
 
 export default cartProductsLoader;
